a451361eb37736e9d9f9fb234e03a26b74a39496
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-08-01 10:59:46 +0200

    add db y conf app.js

---
+++ helpers/connection.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const dbConnect = async () => {
+    try {
+        const res = await mongoose.connect(process.env.URI_DB);
+        console.log('Conectado a la bbdd')
+        return res
+    } catch (error) {
+        return {
+            ok: false,
+            msg: 'Error al conectar la bbdd'
+        }
+    }
+}
+module.exports = {dbConnect};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
