0979cdd2525d2316544768a355f532209a61b766
navarrorafa <115833775+navarrorafa@users.noreply.github.com>
2023-08-01 12:53:58 +0200

    Validator done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,21 @@
 const User = require('../models/userMovie');
+const bcrypt = require('bcryptjs')
 
 
 
 //POST CREAR USER
 const createUser = async (req,res) => {
+
+    const {user, password , nombre} = req.body
+
+    try {
+        
+    } catch (error) {
+        let user = await User.findOne({user:user});
+        console.log(user)
+        
+    }
+   
 
 }
 
--- middleware/vallidation.js
+++ middleware/vallidation.js
@@ -1,7 +1,8 @@
 const {validationResult} = require('express-validator');
 
-const validarJWT = (req,res,next) => {
+const validarEx = (req,res,next) => {
     const errors = validationResult(req);
+
 
     if(!errors.isEmpty()) {
         res.status(400).json({
@@ -13,11 +14,7 @@ const validarJWT = (req,res,next) => {
 };
 
 
-const passwordValidator = check('password')
-check('password')
-.notEmpty().withMessage('Contraseña obligatòria')
-.isLength({ min: 6 }).withMessage('minimo 6 caracteres')
-.matches(/^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/).withMessage('La contraseña debe contener pelo menos 1 mayuscula y 1 numero');
+const passwordValidator =
 
 module.exports={
     validarJWT,
